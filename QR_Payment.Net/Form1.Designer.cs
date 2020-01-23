b2ad880d70a9bc25720930e9f09e4c14a14e9d7e
Arleng2017 <arleng2017@gmail.com>
2020-01-23 18:14:27 +0700

    edit new qrcode generate version

--- QR_Payment.Net/Form1.Designer.cs
+++ QR_Payment.Net/Form1.Designer.cs
@@ -29,21 +29,28 @@
         private void InitializeComponent()
         {
             this.QRBox = new System.Windows.Forms.PictureBox();
-            this.ID_textBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.Amount = new System.Windows.Forms.Label();
-            this.generate_Btn = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.amount_textbox = new System.Windows.Forms.NumericUpDown();
             this.ID = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.copyClipboard_button = new System.Windows.Forms.Button();
+            this.txtTaxID = new System.Windows.Forms.TextBox();
+            this.txtSuffix = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtRef1 = new System.Windows.Forms.TextBox();
+            this.txtRef2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cbUse = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.QRBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.amount_textbox)).BeginInit();
             this.SuspendLayout();
             // 
             // QRBox
             // 
+            this.QRBox.BackColor = System.Drawing.Color.White;
             this.QRBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.QRBox.Location = new System.Drawing.Point(12, 12);
             this.QRBox.Name = "QRBox";
@@ -52,44 +59,19 @@
             this.QRBox.TabIndex = 0;
             this.QRBox.TabStop = false;
             // 
-            // ID_textBox
-            // 
-            this.ID_textBox.Location = new System.Drawing.Point(601, 58);
-            this.ID_textBox.Name = "ID_textBox";
-            this.ID_textBox.Size = new System.Drawing.Size(246, 20);
-            this.ID_textBox.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(535, 61);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(60, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "ID / Phone";
-            // 
             // Amount
             // 
             this.Amount.AutoSize = true;
-            this.Amount.Location = new System.Drawing.Point(535, 87);
+            this.Amount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Amount.Location = new System.Drawing.Point(442, 217);
             this.Amount.Name = "Amount";
-            this.Amount.Size = new System.Drawing.Size(43, 13);
+            this.Amount.Size = new System.Drawing.Size(65, 20);
             this.Amount.TabIndex = 4;
             this.Amount.Text = "Amount";
             // 
-            // generate_Btn
-            // 
-            this.generate_Btn.Location = new System.Drawing.Point(772, 389);
-            this.generate_Btn.Name = "generate_Btn";
-            this.generate_Btn.Size = new System.Drawing.Size(75, 23);
-            this.generate_Btn.TabIndex = 5;
-            this.generate_Btn.Text = "Generate";
-            this.generate_Btn.UseVisualStyleBackColor = true;
-            this.generate_Btn.Click += new System.EventHandler(this.generate_Btn_Click);
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(601, 388);
+            this.button1.Location = new System.Drawing.Point(513, 365);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -97,33 +79,17 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // amount_textbox
-            // 
-            this.amount_textbox.Location = new System.Drawing.Point(601, 84);
-            this.amount_textbox.Name = "amount_textbox";
-            this.amount_textbox.Size = new System.Drawing.Size(246, 20);
-            this.amount_textbox.TabIndex = 7;
-            // 
             // ID
             // 
             this.ID.AutoSize = true;
-            this.ID.Location = new System.Drawing.Point(505, 188);
+            this.ID.Location = new System.Drawing.Point(12, 428);
             this.ID.Name = "ID";
             this.ID.Size = new System.Drawing.Size(0, 13);
             this.ID.TabIndex = 8;
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(437, 188);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(62, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "QR String : ";
-            // 
             // copyClipboard_button
             // 
-            this.copyClipboard_button.Location = new System.Drawing.Point(668, 311);
+            this.copyClipboard_button.Location = new System.Drawing.Point(647, 365);
             this.copyClipboard_button.Name = "copyClipboard_button";
             this.copyClipboard_button.Size = new System.Drawing.Size(112, 23);
             this.copyClipboard_button.TabIndex = 10;
@@ -131,25 +97,137 @@
             this.copyClipboard_button.UseVisualStyleBackColor = true;
             this.copyClipboard_button.Click += new System.EventHandler(this.copyClipboard_button_Click);
             // 
+            // txtTaxID
+            // 
+            this.txtTaxID.Location = new System.Drawing.Point(513, 78);
+            this.txtTaxID.Name = "txtTaxID";
+            this.txtTaxID.Size = new System.Drawing.Size(246, 20);
+            this.txtTaxID.TabIndex = 1;
+            // 
+            // txtSuffix
+            // 
+            this.txtSuffix.Location = new System.Drawing.Point(513, 114);
+            this.txtSuffix.Name = "txtSuffix";
+            this.txtSuffix.Size = new System.Drawing.Size(246, 20);
+            this.txtSuffix.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(447, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "TaxID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(449, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Suffix";
+            // 
+            // txtRef1
+            // 
+            this.txtRef1.Location = new System.Drawing.Point(513, 146);
+            this.txtRef1.Name = "txtRef1";
+            this.txtRef1.Size = new System.Drawing.Size(246, 20);
+            this.txtRef1.TabIndex = 1;
+            // 
+            // txtRef2
+            // 
+            this.txtRef2.Location = new System.Drawing.Point(513, 182);
+            this.txtRef2.Name = "txtRef2";
+            this.txtRef2.Size = new System.Drawing.Size(246, 20);
+            this.txtRef2.TabIndex = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(447, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Ref1";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(447, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Ref2";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(559, 304);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(112, 37);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.generate_Btn_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(513, 217);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(246, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // cbUse
+            // 
+            this.cbUse.FormattingEnabled = true;
+            this.cbUse.Items.AddRange(new object[] {
+            "single",
+            "multi"});
+            this.cbUse.Location = new System.Drawing.Point(513, 254);
+            this.cbUse.Name = "cbUse";
+            this.cbUse.Size = new System.Drawing.Size(246, 21);
+            this.cbUse.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(456, 255);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "use";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1029, 450);
+            this.ClientSize = new System.Drawing.Size(804, 450);
+            this.Controls.Add(this.cbUse);
             this.Controls.Add(this.copyClipboard_button);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.ID);
-            this.Controls.Add(this.amount_textbox);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.generate_Btn);
-            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.ID_textBox);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.txtRef2);
+            this.Controls.Add(this.txtRef1);
+            this.Controls.Add(this.txtSuffix);
+            this.Controls.Add(this.txtTaxID);
             this.Controls.Add(this.QRBox);
             this.Name = "Form1";
             this.Text = "PromptPayQR";
             ((System.ComponentModel.ISupportInitialize)(this.QRBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.amount_textbox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,15 +236,22 @@
         #endregion
 
         private System.Windows.Forms.PictureBox QRBox;
-        private System.Windows.Forms.TextBox ID_textBox;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label Amount;
-        private System.Windows.Forms.Button generate_Btn;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.NumericUpDown amount_textbox;
         private System.Windows.Forms.Label ID;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button copyClipboard_button;
+        private System.Windows.Forms.TextBox txtTaxID;
+        private System.Windows.Forms.TextBox txtSuffix;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtRef1;
+        private System.Windows.Forms.TextBox txtRef2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox cbUse;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- QR_Payment.Net/Form1.cs
+++ QR_Payment.Net/Form1.cs
@@ -22,18 +22,43 @@ namespace QR_Payment.Net
 
         private void generate_Btn_Click(object sender, EventArgs e)
         {
-            var promptpayID = ID_textBox.Text;
-            var amount = amount_textbox.Value.ToString();
-            if (amount.Contains("0"))
-            {
-                amount = "";
+           
+            var EMVCo = "000201";
+            var qrUse = (cbUse.Text=="single")? "010212" : "010211";
+            var applicationID = "0016A000000677010112";
+            var billPaymentTaxID = txtTaxID.Text;
+            var billPaymentSuffix = txtSuffix.Text;
+            var BillerID = $"{billPaymentTaxID}{billPaymentSuffix}";
+            var billPaymentBillerID = $"01{len(BillerID)}{BillerID}";
+            var reference1 = $"02{len(txtRef1.Text)}{txtRef1.Text}";
+            var reference2 = $"03{len(txtRef2.Text)}{txtRef2.Text}";
+            var billPayment = $"{applicationID}{billPaymentBillerID}{reference1}{reference2}";
+            var billPaymentInfo = $"30{len(billPayment)}{billPayment}";
+            var transactionCurrency = "5303764";
+            var transactionAmount = "";
+            if (!String.IsNullOrEmpty(txtAmount.Text)) {
+                var amount = String.Format("{0:0.00}", Convert.ToDouble(txtAmount.Text));
+                transactionAmount = $"54{len(amount)}{amount}";
             }
-
-            //var qrCode = PromptPayQR.QRCodePayload(promptpayID, amount);
-            var qrCode = PromptPayQR.PromptPayQR.QRCodePayload(promptpayID, amount);
+            else {
+                transactionAmount = "";
+            }
+            var countryCode = "5802TH";
+            var qrcode = $"{EMVCo}{qrUse}{billPaymentInfo}{transactionCurrency}{transactionAmount}{countryCode}6304";
+            var payload = qrcode.ToString().Select(x => Convert.ToByte(x)).ToArray();
+            var crc = new CRC16().ComputeCheckSum(payload);
+            var crc16 = crc.ToString("X").PadLeft(4, '0');
+            qrcode = $"{qrcode}{crc16}";
+            var qrCode = PromptPayQR.PromptPayQR.QRCodePayload(qrcode);
             ID.Text = qrCode;
             var imageQR = PromptPayQR.PromptPayQR.QRCodeImage(qrCode);
             QRBox.Image = imageQR;
+            subText();
+        }
+        public static string len(string str)
+        {
+            var strLen=(str.Length < 10) ? $"0{str.Length}" : str.Length.ToString();
+            return strLen;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -50,7 +75,14 @@ namespace QR_Payment.Net
                 }
             }
         }
+        public static void subText() {
+            var str = "00020101021130550016A00000067701011201150135560022933020204ref10304ref253037645802TH63048FC2";
+            var li = new List();
+            for(int i=0;i<str.Length;i++){
+                Console.WriteLine(str[i]);
 
+            }
+        }
         private void copyClipboard_button_Click(object sender, EventArgs e)
         {
             Clipboard.SetText(ID.Text);
