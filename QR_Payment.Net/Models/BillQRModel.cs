6e7fe0f650ade7f59d9733499c28798707b374b7
chokchai9900 <chokchai9900@gmail.com>
2020-02-18 11:32:01 +0700

    add decode class and QR Model

---
+++ QR_Payment.Net/Models/BillQRModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Payment.Net.Models
+{
+    class BillQRModel
+    {
+    }
+}
