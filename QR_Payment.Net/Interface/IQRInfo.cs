fc68a9cf1db204462aa079d280e62a3dd3bd1a3c
chokchai9900 <chokchai9900@gmail.com>
2020-02-19 10:09:08 +0700

    add Interface

---
+++ QR_Payment.Net/Interface/IQRInfo.cs
@@ -0,0 +1,24 @@
+﻿using QR_Payment.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QR_Payment.Net.Interface
+{
+    public interface IQRInfo
+    {
+        string CreateQRString(QRData qrData);
+        QRInfo QRInfomation(string QRString);
+    }
+
+    public class QRData
+    {
+    }
+
+    public class QRInfo
+    {
+
+    }
+}
